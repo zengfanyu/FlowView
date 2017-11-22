8bbe5c38a18a7bc4af3ffc42485206238c29b36d
fanyuzeng <393568551@qq.com>
2017-11-22 16:54:20 +0800

    重写FlowLayout的onLayout方法,按照流式布局的思路,确定子View的位置

--- app/src/main/java/com/project/fanyuzeng/flowview/FlowLayout.java
+++ app/src/main/java/com/project/fanyuzeng/flowview/FlowLayout.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: fanyuzeng on 2017/11/22 10:19
  * @desc:
@@ -33,6 +36,7 @@ public class FlowLayout extends ViewGroup {
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+//        Log.d(TAG,">> onMeasure >> " + "widthMode:"+widthMode+",heightMode:"+heightMode);
 
         //适配 wrap_content 情况
 
@@ -46,17 +50,17 @@ public class FlowLayout extends ViewGroup {
         int lineHeight = 0;
 
         int childCount = getChildCount();
-
+        Log.d(TAG, ">> onMeasure >> " + "childCount:" + childCount);
         for (int i = 0; i < childCount; i++) {
-            View childAt = getChildAt(i);
+            View childView = getChildAt(i);
             //测量子View 的宽高
-            measureChild(childAt, widthMeasureSpec, heightMeasureSpec);
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
             //子View的 LayoutParams
-            MarginLayoutParams lp = (MarginLayoutParams) childAt.getLayoutParams();
+            MarginLayoutParams lp = (MarginLayoutParams) childView.getLayoutParams();
 
             //子View占据的宽高 等于 其本身的宽高加上左右 Margin 值
-            int childWidth = childAt.getMeasuredHeight() + lp.leftMargin + lp.rightMargin;
-            int childHeight = childAt.getMeasuredWidth() + lp.topMargin + lp.bottomMargin;
+            int childWidth = childView.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childHeight = childView.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
 
             //当前行的宽度在加上一个子View的宽度，比父View的宽度要大，那么就需要换行了
             if (lineWidth + childWidth > widthSize) {
@@ -79,20 +83,104 @@ public class FlowLayout extends ViewGroup {
                 width = Math.max(width, lineWidth);
                 height += lineHeight;
             }
+            Log.d(TAG, ">> onMeasure >> " + "height:" + height);
 
         }
 
-        Log.d(TAG, ">> onMeasure >> " + "widthSize:" + widthSize + ",heightSize:" + heightSize);
-        setMeasuredDimension(widthMode == MeasureSpec.EXACTLY ? widthSize : width, heightMode == MeasureSpec.AT_MOST ? heightSize : height);
 
-
-
-
+//        Log.d(TAG, ">> onMeasure >> " + "widthSize:" + widthSize + ",heightSize:" + heightSize);
+        setMeasuredDimension(widthMode == MeasureSpec.EXACTLY ? widthSize : width, heightMode == MeasureSpec.EXACTLY ? heightSize : height);
 
     }
 
+    /**
+     * 一行一行的存储所有的控件
+     */
+    private List<List<View>> mChildViewList = new ArrayList<>();
+
+    /**
+     * 每一行高度
+     */
+    private List<Integer> mLineHeighValueList = new ArrayList<>();
+
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mChildViewList.clear();
+        mLineHeighValueList.clear();
+        //拿到当前ViewGroup的宽度
+        int width = getWidth();
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+        List<View> lineViews = new ArrayList<>();
+
+        int childCount = getChildCount();
+
+        for (int i = 0; i < childCount; i++) {
+            View childView = getChildAt(i);
+            MarginLayoutParams lp = (MarginLayoutParams) childView.getLayoutParams();
+            int childViewWidth = childView.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childViewHeight = childView.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+
+            //当前行宽加上 下一个子View的宽度 大于父容器的宽度时 换行
+            if (lineWidth + childViewWidth > width) {
+                //记录上一行的高度值
+                mLineHeighValueList.add(lineHeight);
+                //记录上一行的所有View
+                mChildViewList.add(lineViews);
+
+                lineWidth = 0;
+                lineHeight = childViewHeight;
+                //重置集合
+                lineViews = new ArrayList<>();
+
+            }
+            //不换行
+            lineWidth += childViewWidth;
+            lineHeight = Math.max(lineHeight, childViewHeight);
+            lineViews.add(childView);
+        }
+        mLineHeighValueList.add(lineHeight);
+        mChildViewList.add(lineViews);
+
+
+        //设置子View的位置
+        int left = 0;
+        int top = 0;
+
+        int lineNum = mChildViewList.size();
+
+        for (int i = 0; i < lineNum; i++) {
+            //当前行的所有View
+            lineViews = mChildViewList.get(i);
+            lineHeight = mLineHeighValueList.get(i);
+
+            for (int j = 0; j < lineViews.size(); j++) {
+                View view = lineViews.get(j);
+
+                //判断当前子View的状态
+                if (view.getVisibility() == GONE) {
+                    continue;
+                }
+
+                MarginLayoutParams lp = (MarginLayoutParams) view.getLayoutParams();
+
+                int lc = left + lp.leftMargin;
+                int tc = top + lp.topMargin;
+                int rc = lc + view.getMeasuredWidth();
+                int bc = tc + view.getMeasuredHeight();
+
+                //对子View进行布局
+                view.layout(lc, tc, rc, bc);
+
+                left += view.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+
+            }//这个 for 循环是用于布局某一行的所有view
+
+            left = getPaddingLeft();
+            top += lineHeight;
+        }
 
     }
 
--- app/src/main/java/com/project/fanyuzeng/flowview/MainActivity.java
+++ app/src/main/java/com/project/fanyuzeng/flowview/MainActivity.java
@@ -1,16 +1,44 @@
 package com.project.fanyuzeng.flowview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 /**
  * @author fanyuzeng
  */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private FlowLayout mFlowLayout;
+    private int mFlowLayoutHeight;
+    private int mFlowLayoutWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFlowLayout = (FlowLayout) findViewById(R.id.id_flow_layout);
+
+
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+
+
 }
